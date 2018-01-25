83e90f9cf8968955d1eb0caa37bee6e6a9dd52bb
Jman2427 <222170@c1mrxe7nh3qd.isd720.com>
2018-01-25 13:09:26 -0600

    Added Strategy Name
    Added new strategy name and description.

--- team11.py
+++ team11.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Meeseeks' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Copycat'
+strategy_description = 'Copy opponents most recent move'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
